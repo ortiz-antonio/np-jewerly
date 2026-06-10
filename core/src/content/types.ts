8dc05edfe4922ccdfb20791f9303e807f3105941
Antonio Ortiz <ortiz.antonio@gmail.com>
2026-06-10 15:47:02 -0500

    refactor for name sofia with argollas

--- core/src/content/types.ts
+++ core/src/content/types.ts
@@ -2,5 +2,12 @@ export interface HeroData {
   title: string
   text: string
   image: string
+  imageProduction: string
+  imageDownload: string
   font: string
+}
+
+export interface SubscriptionData {
+  title: string
+  text: string
 }
